f68b36a3708eadb4675dfdb890f782069118dcf0
Thomas O'Brien <thomasobrien99@gmail.com>
2016-06-11 12:02:48 -0700

    start to convert to app with back end. begin to integrate database

---
+++ server/migrations/20160611113109_ADD_TEAS_TABLE.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('teas', table=>{
+    table.string('_id');
+    table.string('name');
+    table.string('ingredients');
+    table.integer('caffeineScale');
+    table.integer('price');
+    table.boolean('inStock');
+    table.integer('rating');
+    table.text('imageUrl');
+    table.integer('__v');
+    table.json('categories');
+    table.integer('quantity');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('teas');
+};
